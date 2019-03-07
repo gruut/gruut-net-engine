0a972b674bf794ce051f414d428f2bac8274c78f
blackiron <jjj@gruut.net>
2019-03-07 16:48:41 +0900

    # ADD - Sender class 추가
    - 메시지를 전송하는 객체
    - 현재는 FIND_NODE, PING rpc로 rpc_server에 request를 보내는 부분 구현

--- network/sender.cpp
+++ network/sender.cpp
@@ -1,24 +1,25 @@
 #include "sender.hpp"
-#include "http_client.hpp"
 
 namespace gruut{
 namespace net{
 
-std::unique_ptr<KademliaService::Stub> Sender::genKademliaStub(std::string &addr, std::string &port) {
+std::unique_ptr<KademliaService::Stub> Sender::genKademliaStub(const std::string &addr, const std::string &port) {
 
   auto credential = InsecureChannelCredentials();
   auto channel = CreateChannel(addr + ":" + port, credential);
   return KademliaService::NewStub(channel);
 }
 
-PongData Sender::pingReq(std::string &receiver_addr, std::string &receiver_port) {
+PongData Sender::pingReq(const std::string &receiver_addr, const std::string &receiver_port) {
 
   auto stub = genKademliaStub(receiver_addr, receiver_port);
   ClientContext context;
 
+  //TODO : Time stamp 값 현재 시간값 으로 변경해야함.
   Ping ping;
   ping.set_time_stamp(0);
   ping.set_node_id(MY_ID);
+  ping.set_version(1);
   ping.set_sender_address(IP_ADDRESS);
   ping.set_sender_port(DEFAULT_PORT_NUM);
 
@@ -33,8 +34,8 @@ PongData Sender::pingReq(std::string &receiver_addr, std::string &receiver_port)
   return ret;
 }
 
-NeighborsData Sender::findNodeReq(std::string &receiver_addr,
-                                  std::string &receiver_port,
+NeighborsData Sender::findNodeReq(const std::string &receiver_addr,
+                                  const std::string &receiver_port,
                                   const Node::IdType &target_id) {
 
   auto stub = genKademliaStub(receiver_addr, receiver_port);
--- network/sender.hpp
+++ network/sender.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
-#include "rpc_service/rpc_services.hpp"
-#include "p2p/node.hpp"
-#include "message.hpp"
+#include "rpc_services/rpc_services.hpp"
 #include <vector>
 
 namespace gruut{
@@ -12,20 +10,11 @@ class Sender{
 public:
   Sender() = default;
 
-  void sendToMerger();
-  void sendToSigner();
-
-  void sendHttpReq(std::string &address, );
-
-  PongData pingReq(std::string &receiver_addr, std::string &receiver_port);
-  NeighborsData findNodeReq(std::string &receiver_addr, std::string &receiver_port, const Node::IdType &target_id);
+  PongData pingReq(const std::string &receiver_addr, const std::string &receiver_port);
+  NeighborsData findNodeReq(const std::string &receiver_addr, const std::string &receiver_port, const Node::IdType &target_id);
 
 private:
-
-  bool checkMergerMsgType(MessageType msg_type);
-  bool checkSignerMsgType(MessageType msg_type);
-
-  std::unique_ptr< KademliaService::Stub> genKademliaStub(std::string &addr, std::string &port);
+  std::unique_ptr< KademliaService::Stub> genKademliaStub(const std::string &addr, const std::string &port);
 
 };
 
